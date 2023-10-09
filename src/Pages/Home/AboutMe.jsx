56a1031e9c68a7132b043c677ab0ad9bf90e18d5
FarhanDevelopeir <fb4459467@gmail.com>
2023-10-08 22:45:03 -0700

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/abc.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I'm Muhammad Farhan, a React developer and front-end lover with an IT degree. I make cool websites that people enjoy using. I'm great at solving problems and writing clean code. Let's work together to build awesome web stuff!
+          </p>
+          <p className="hero--section-description">
+          
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Assalamoalaikum! I'm Muhammad Farhan</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">React </span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Experienced React developer dedicated to transforming design concepts into functional web solutions.
+            <br /> 
+          </p>
+        </div>
+        {/* <button className="btn btn-primary">Get In Touch</button> */}
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/abcr.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
